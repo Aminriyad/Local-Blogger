ad758964861ebeaec5c9eb5e80e47dbc9d0050a4
amin riyad <aminriyad@hotmail.com>
2022-11-10 11:47:52 -0600

    main

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code><a href="https:\\aminriyad.com" target="_blank"> aminriyad</a></code> what is up ma Nigga.
         </p>
         <a
           className="App-link"
