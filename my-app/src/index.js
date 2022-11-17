0eac83a24410a40768849dd6ad64a2d23b2ac503
aminriyad <aminriyad@hotmail.com>
2022-11-17 07:27:15 -0600

    supmitteed after work

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -7,7 +7,7 @@ class Amin extends React.Component{
     isit: "Click increment to see if it divided by two",
   }
   increment = () => {
-    this.setState({age: this.state.age-1});
+    this.setState({age: this.state.age+1});
     if(this.state.age %2){
       this.setState({second:"is divided by two"});
     document.getElementById('amin').style.backgroundColor = "red"}
@@ -15,14 +15,24 @@ class Amin extends React.Component{
     this.setState({second:"is not divided by two"})
     document.getElementById('amin').style.backgroundColor = "blue";}
 }
-
+decrement = () => {
+  this.setState({age: this.state.age-1});
+  if(this.state.age %2){
+    this.setState({second:"is divided by two"});
+  document.getElementById('aminer').style.backgroundColor = "red"}
+  else{
+  this.setState({second:"is not divided by two"})
+  document.getElementById('aminer').style.backgroundColor = "blue";};
+}
   render() {
-    return <div>
+    return <div className='holder'>
     <p>{this.state.second}</p>
     <p>{this.state.age}</p>
     <button onClick={this.increment} className="amin" id='amin'>Increment</button>
+    <button onClick={this.decrement} className="amin" id='aminer'>Decrement</button>
     </div>;
   }
+
 }
 const el = <Amin/>
 ReactDOM.render(
