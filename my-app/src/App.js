4318976c5523ffe484d0513c19b6484b721b486f
aminriyad <aminriyad@hotmail.com>
2022-12-11 03:06:01 -0600

    main

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,21 @@
-import React from "react"
+import React from "react";
 import Aminer from './Aminer';
 import './index.css'
 const Amin = () => {
-  state = {
+  var state = {
     age : 18,
-    isit: "Click increment to see if it divided by two",
-  }
-  increment = () => {
+    isit: "Click increment to see if it divided by two"
+  };
+  var increment = () => {
     this.setState({age: this.state.age+1});
-    if(this.state.age %2){
+    if(this.state.age % 2){
       this.setState({second:"is divided by two"});
     document.getElementById('amin').style.backgroundColor = "red"}
     else{
     this.setState({second:"is not divided by two"})
     document.getElementById('amin').style.backgroundColor = "blue";}
-}
-decrement = () => {
+};
+var decrement = () => {
   this.setState({age: this.state.age-1});
   if(this.state.age %2){
     this.setState({second:"is divided by two"});
@@ -23,17 +23,17 @@ decrement = () => {
   else{
   this.setState({second:"is not divided by two"})
   document.getElementById('aminer').style.backgroundColor = "blue";};
-}
-  render() {
-    return <div className='holder'>
-    <p>{this.state.second}</p>
-    <p>{this.state.age}</p>
-    <button onClick={this.increment} className="amin" id='amin'>Increment</button>
-    <button onClick={this.decrement} className="amin" id='aminer'>Decrement</button>
-   <Aminer/>
-    </div>;
-  }
+};
 
-}
+    return( <div className='holder'>
+    <p>{state.second}</p>
+    <p>{state.age}</p>
+    <button onClick={increment} className="amin" id='amin'>increment</button>
+    <button onClick={decrement} className="amin" id='aminer'>decrement</button>
+    <Aminer/>
+    </div>)
+    }
+
+
 
 export default Amin;
